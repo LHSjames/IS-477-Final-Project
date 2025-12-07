b979ba0642eb09ff13bdac7537c00f61c99b10e3
LHSjames <hosangl2@illinois.edu>
2025-12-07 13:53:07 -0600

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+# 1. Stop the script immediately if any notebook fails (Critical for grading!)
+set -e
+
 echo "Starting Data Integration..."
-jupyter nbconvert --to notebook --execute 01_data_integration.ipynb
+jupyter nbconvert --to notebook --execute --inplace 01_data_integration.ipynb
 
 echo "Starting Data Cleaning..."
-jupyter nbconvert --to notebook --execute 02_cleaning.ipynb
+jupyter nbconvert --to notebook --execute --inplace 02_cleaning.ipynb
 
 echo "Starting EDA..."
-jupyter nbconvert --to notebook --execute EDA.ipynb
+jupyter nbconvert --to notebook --execute --inplace EDA.ipynb
 
 echo "Starting Modeling..."
-jupyter nbconvert --to notebook --execute 03_modeling.ipynb
+jupyter nbconvert --to notebook --execute --inplace 03_modeling.ipynb
 
-echo "Workflow Complete!"
+echo "Workflow Complete! All notebooks ran successfully."
